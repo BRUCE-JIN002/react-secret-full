cd41f8a7958a66c2508356089e1cf988dd216492
jinxianxiang <1633764493@qq.com>
2024-05-23 00:42:30 +0800

    Form&&Upload components finished

--- src/components/form/FormDemo.tsx
+++ src/components/form/FormDemo.tsx
@@ -1,5 +1,6 @@
 import { Button, Checkbox, Input } from "antd";
 import Form from "./index";
+import Space from "../space/Space";
 
 const Basic: React.FC = () => {
   const onFinish = (values: any) => {
@@ -15,36 +16,40 @@ const Basic: React.FC = () => {
       initialValues={{ remember: true, username: "测试名字" }}
       onFinish={onFinish}
       onFinishFailed={onFinishFailed}
+      className="w-[300px]"
     >
-      <Form.Item
-        label="Username"
-        name="username"
-        rules={[
-          { required: true, message: "请输入用户名!" },
-          { max: 6, message: "长度不能大于 6" },
-        ]}
-      >
-        <Input />
-      </Form.Item>
-
-      <Form.Item
-        label="Password"
-        name="password"
-        rules={[{ required: true, message: "请输入密码!" }]}
-      >
-        <Input />
-      </Form.Item>
-
-      <Form.Item name="remember" valuePropName="checked">
-        <Checkbox>记住我</Checkbox>
-      </Form.Item>
+      <Space direction="vertical" size={8} className="w-full">
+        <Form.Item
+          label="Username"
+          name="username"
+          rules={[
+            { required: true, message: "请输入用户名!" },
+            { max: 6, message: "长度不能大于 6" },
+          ]}
+        >
+          <Input />
+        </Form.Item>
+        <Form.Item
+          label="Password"
+          name="password"
+          rules={[{ required: true, message: "请输入密码!" }]}
+        >
+          <Input />
+        </Form.Item>
+        <Form.Item name="remember" valuePropName="checked">
+          <Checkbox>记住密码</Checkbox>
+        </Form.Item>
+      </Space>
 
       <Form.Item>
-        <div>
-          <Button type="primary" size="small" htmlType="submit">
-            登录
-          </Button>
-        </div>
+        <Button
+          type="primary"
+          size="small"
+          htmlType="submit"
+          className="w-full mt-8"
+        >
+          登录
+        </Button>
       </Form.Item>
     </Form>
   );
