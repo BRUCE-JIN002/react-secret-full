cb12e7b2c7007f52eee7a5f0a5bb3652abc764c5
jinxianxiang <1633764493@qq.com>
2024-06-18 00:20:23 +0800

    add some custom react hooks

---
+++ src/hooks/useUnmountedRef.ts
@@ -0,0 +1,17 @@
+import { useEffect, useRef } from "react";
+
+const useUnmountedRef = (): { readonly current: boolean } => {
+  const unMountedRef = useRef<boolean>(false);
+
+  useEffect(() => {
+    unMountedRef.current = false;
+
+    return () => {
+      unMountedRef.current = true;
+    };
+  }, []);
+
+  return unMountedRef;
+};
+
+export default useUnmountedRef;
